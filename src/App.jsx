bdbf3f3eb9b6e1a260f440c3b3e6e598cf63a4f8
TimGielen <Tim.Gielen2@student.pxl.be>
2022-05-02 14:14:09 +0200

    edit app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import useDarkMode from 'use-dark-mode';
+import AppContext from './AppContext';
+import MainApp from './MainApp';
+import GlobalStyles from './theme/GlobalStyles';
+import { lightTheme, darkTheme } from './theme/themes';
 
 function App() {
+  window.matchMedia = null;
+  const darkMode = useDarkMode(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider value={{ darkMode }}>
+        <GlobalStyles />
+        <div className="App">
+          <BrowserRouter>
+            <MainApp />
+          </BrowserRouter>
+        </div>
+      
+    </AppContext.Provider>
   );
 }
 
